1041edf9f806b73fff389585c172f5438a7e7a76
verm024 <muhnaufal251@gmail.com>
2020-11-10 19:34:07 +0700

    Initial config

--- balistay/src/main.js
+++ balistay/src/main.js
@@ -1,12 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import { firestorePlugin } from "vuefire";
+import firebase from "./firebase";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(firestorePlugin);
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+let app;
+firebase.auth.onState;
+
+firebase.auth.onAuthStateChanged(async user => {
+  if (user) {
+    store.commit("setCurrentUser", user);
+    store.dispatch("fetchUserProfile");
+  } else {
+    store.commit("setCurrentUser", null);
+    store.commit("setUserProfile", null);
+  }
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount("#app");
+  }
+});
--- balistay/src/store/index.js
+++ balistay/src/store/index.js
@@ -1,15 +1,41 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import firebase from "../firebase";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    currentUser: null,
+    userProfile: null
   },
   mutations: {
+    setCurrentUser(state, val) {
+      state.currentUser = val;
+    },
+    setUserProfile(state, val) {
+      state.userProfile = val;
+    }
   },
   actions: {
+    async fetchUserProfile({ commit, state }) {
+      let tempUserProfile = await firebase.db
+        .collection("users")
+        .doc(state.currentUser.uid)
+        .get();
+      let data = tempUserProfile.data();
+      commit("setUserProfile", data);
+    }
   },
-  modules: {
-  }
-})
+  modules: {},
+  getters: {
+    currentUser: state => {
+      return state.currentUser;
+    },
+    userProfile: state => {
+      return state.userProfile;
+    }
+  },
+  plugins: [createPersistedState()]
+});
