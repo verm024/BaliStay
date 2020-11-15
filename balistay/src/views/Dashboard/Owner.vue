d2a5d5521e4ba8bcd4afa9ce193febf57e6bde46
verm024 <muhnaufal251@gmail.com>
2020-11-16 00:03:59 +0700

    Dashboard owner

--- balistay/src/views/Dashboard/Owner.vue
+++ balistay/src/views/Dashboard/Owner.vue
@@ -1,14 +1,43 @@
 <template>
-  <div class="owner">
-    Dashboard Owner
-    <router-link to="/new-homestay">
-      New Homestay
-    </router-link>
-    <div>
-      <div v-for="(item, index) in daftar_penginapan" :key="index">
-        {{ item.nama_penginapan }}
-      </div>
-    </div>
+  <div class="owner container">
+    <v-card class="card-info" elevation="0" tile>
+      <v-card-title class="justify-space-between">
+        <div class="title">
+          Homestay List
+        </div>
+        <div class="button">
+          <router-link to="/new-homestay">
+            <v-btn class="white--text ml-5" tile color="#0EBEE4" elevation="0">
+              New Homestay
+            </v-btn>
+          </router-link>
+        </div>
+      </v-card-title>
+      <v-card-text>
+        <v-card class="card-list text-center pb-10" outlined>
+          <v-card-text>
+            <div class="card-list-text">
+              <div
+                class="mx-5 mr-5 mt-10"
+                v-for="(item, index) in daftar_penginapan"
+                :key="index"
+              >
+                <router-link :to="'/homestay/' + item.id">
+                  <v-card width="250">
+                    <v-img :src="item.image" height="150"></v-img>
+                    <v-card-text class="text-left">
+                      <span class="nama-penginapan">{{
+                        item.nama_penginapan
+                      }}</span>
+                    </v-card-text>
+                  </v-card>
+                </router-link>
+              </div>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
@@ -45,4 +74,32 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.title {
+  font-weight: normal;
+}
+
+.button a {
+  text-decoration: none;
+}
+
+.button .v-btn {
+  text-transform: capitalize;
+  letter-spacing: unset;
+  font-weight: normal;
+}
+
+.card-list {
+  margin-top: 50px;
+}
+
+.card-list-text div {
+  display: inline-block;
+}
+
+.nama-penginapan {
+  font-weight: bold;
+  font-size: 14px;
+  color: rgba(0, 0, 0, 0.4);
+}
+</style>
